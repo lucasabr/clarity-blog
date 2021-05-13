af84573355260a0f0313abe6eb59491263050ea6
lucasabr <lucasabr2001@gmail.com>
2021-05-13 18:19:43 -0400

    ejs
    ejs/nodemon

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('/express');
 
 const app = express();
 
+app.set('view-engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
