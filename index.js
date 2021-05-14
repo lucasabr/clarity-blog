30240cf6ee5c0cd75f4c68952b35ba575d0b4803
lucasabr <lucasabr2001@gmail.com>
2021-05-14 16:36:04 -0400

    Created basic (no css) register/login
    Yet to handle post requests

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ app.set('view-engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
