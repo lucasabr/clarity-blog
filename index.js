b076ed33e8e6bfc9af8d36553204ba08428a0a7a
lucasabr <lucasabr2001@gmail.com>
2021-05-15 21:10:01 -0400

    setup mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const express = require('/express');
-
+const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+main();
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    const uri = "mongodb+srv://lucasabr:<topsecretpassword123>@lucas-database.ckz0z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        await  listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
 
 app.set('view-engine', 'ejs');
 
