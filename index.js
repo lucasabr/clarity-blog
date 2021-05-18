9b7a504f691264399cec4667d586c7ca2b5b05c7
lucasabr <lucasabr2001@gmail.com>
2021-05-18 00:37:21 -0400

    Started working on passport

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+if (process.env.NODE_ENV !== 'production') { 
+    require('dotenv').config()
+}
+
 //Basic App Init
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bcrypt = require('bcrypt');
+const flash = require('express-flash')
+const session = require('express-session')
+//Passport Init
+const passport = require('passport');
+const initializePass = require('./pass-config')
+initializePass(passport, 
+    email => user.findOne({email: email}),
+    id => user.findOne({id: id})
+);
 //Database Init
 const mongoose = require('mongoose');
 const User =  require('./models/user');
@@ -15,6 +28,14 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 
 
 app.set('view-engine', 'ejs');
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -24,9 +45,11 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
-    
-})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+})) 
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
@@ -36,18 +59,15 @@ app.post('/register', async (req, res) => {
     try{
         const hashedPassword = await bcrypt.hash('req.body.password', 10)
         const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
+            name: 'req.body.name',
+            email: 'req.body.email',
+            password: 'hashedPassword'
         });
         user.save()
             .then((result) => res.redirect('/login'))
             .catch((err) => console.log(err))
-    } catch {
-        console.log('error');
+    } catch (e){
+        console.log(e);
         res.redirect('/register')
-    }
-    
-
-    
+    }  
 })
