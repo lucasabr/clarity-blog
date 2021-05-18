5cc644f12aa6c9fb1aa089058e57bb4779ab445e
lucasabr <lucasabr2001@gmail.com>
2021-05-17 21:49:36 -0400

    Added models/Created register post request/Fixed Ejs

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//Basic App Init
 const express = require('express');
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://lucasabr:topsecretpassword123@lucas-database.ckz0z.mongodb.net/website-test?retryWrites=true&w=majority";
-
-
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+const bcrypt = require('bcrypt');
+//Database Init
+const mongoose = require('mongoose');
+const User =  require('./models/user');
+const uri = "mongodb+srv://lucasabr:topsecretpassword123@lucas-database.ckz0z.mongodb.net/website-test?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => app.listen(PORT))
     .catch((err) => console.log(err))
+
 
 
 app.set('view-engine', 'ejs');
@@ -30,5 +33,21 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash('req.body.password', 10)
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        user.save()
+            .then((result) => res.redirect('/login'))
+            .catch((err) => console.log(err))
+    } catch {
+        console.log('error');
+        res.redirect('/register')
+    }
+    
+
     
 })
