36f2756f68d8646840c60c09f1babcacf3fab3c2
lucasabr <lucasabr2001@gmail.com>
2021-05-22 19:29:11 -0400

    Updated register post request, and added README

---
+++ README.md
@@ -0,0 +1,18 @@
+Blog Website (Work In Progress)
+
+This project is done using the MERN stack.
+Currently there is only a registration/login feature.
+
+To do:
+-React Front End
+-Add blog posts
+-Profile page
+
+
+Usage of multiple packages in node.js like:
+
+-Express
+-Passport
+-ejs
+-bcrypt
+-mongoose
--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const PORT = process.env.PORT || 5000;
 const bcrypt = require('bcrypt');
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 var bodyParser = require('body-parser')
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('source'));
+
 //Database Init
 const mongoose = require('mongoose');
 const User =  require('./models/user');
@@ -38,8 +40,8 @@ app.use(session({
 }));
 app.use(passport.initialize())
 app.use(passport.session())
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(methodOverride('_method'))
+
 
 
 
@@ -58,24 +60,51 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 })) 
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
+    res.render('register.ejs', {message: "You must register to access many of the sites features"})
 })
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        user.save()
-            .then((result) => res.redirect('/login'))
-            .catch((err) => console.log(err))
-    } catch (e){
-        console.log(e);
-        res.redirect('/register')
-    }  
+app.post('/register', checkNotAuthenticated, (req, res) => {
+    //looks for a user with the email
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(!user){
+                //if there is no user with the email, looks for a user with the same username
+                User.findOne({name: req.body.name})
+                    .then(async user2 => {
+                        if(!user2){
+                            //if there is no user with the username, saves to database
+                            try{
+                                const hashedPassword = await bcrypt.hash(req.body.password, 10);
+                                const user = new User({
+                                    name: req.body.name,
+                                    email: req.body.email,
+                                    password: hashedPassword
+                                });
+                                user.save()
+                                    .then((result) => res.redirect('/login'))
+                                    .catch((err) => console.log(err))
+                            } catch (e){
+                                console.log(e);
+                                res.redirect('/register')
+                            }
+                        } else{
+                            res.render("register.ejs", {message: "ERROR! There already exists an account with this username"})
+                        }
+                        })
+                        .catch(e => console.log(e))
+                
+        } else{
+            res.render("register.ejs", {message: "ERROR! There already exists an account with this email"})
+        }
+    })
+        .catch( e => console.log(e))
+        
+      
+})
+
+app.delete('/logout', (req,res) => {
+    req.logOut()
+    res.redirect('/login')
 })
 
 function checkAuthenticated(req, res, next) {
@@ -84,6 +113,6 @@ function checkAuthenticated(req, res, next) {
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) res.redirect('./')
+    if(req.isAuthenticated()) return res.redirect('./')
     next()
 }
